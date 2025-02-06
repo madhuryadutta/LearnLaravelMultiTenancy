ac88a0e123df16fabaeaaabacf78df2e6ad0658d
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2025-02-07 03:06:22 +0530

    Added missing code

--- routes/tenant.php
+++ routes/tenant.php
@@ -27,7 +27,13 @@ use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 //         return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
 //     });
 // });
-Route::get('/', function () {
-    dd(\App\Models\User::all());
-    return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+Route::middleware([
+    'web',
+    InitializeTenancyByDomain::class,
+    PreventAccessFromCentralDomains::class,
+])->group(function () {
+    Route::get('/', function () {
+        dd(\App\Models\User::all());
+        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+    });
 });
